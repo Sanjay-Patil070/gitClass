bcfa6fe0537de621cee909a761debf61f5870991
Sanjay Patil <sanjaypatil16786@gmail.com>
2023-08-07 17:26:07 +0530

    staging to localRepo

--- add.java
+++ add.java
@@ -2,7 +2,7 @@
     public static void main(String[] args) {
         int a=10;
         int b=12;
-        System.out.println(a+b);
+        System.out.println("the sum is " + (a+b));cmd
     }
 
     
